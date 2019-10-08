f557b736f067d67edbff303ee1007da86ef748e9
Nattyeah <53342856+Nattyeah@users.noreply.github.com>
2019-10-07 22:26:24 -0300

    Add files via upload
    1º ex  Ju

---
+++ ex.js
@@ -0,0 +1,33 @@
+//1. Percorrer array e printar valor no console
+// let array = [1,5,8,10];
+// for (let i = 0; i < array.length; i++) {
+    
+// }
+// console.log(array)
+
+//2. Inverter ordem do for e printar valores
+//   let array = [1,5,8,10];
+//     for (let i = array.length -1; i >= 0; i--)
+    
+// console.log(array[i]);
+
+//3. Localizar índice de valor da array
+// let array = [1,5,8,10];
+// for (let i = 0; i < array.length; i++){
+
+// if(array[i] === 8) {
+//     console.log(i);
+// }
+// }
+
+  
+//4. Escreva uma função comprimentoMedioPalavras que receba duas strings e devolva o comprimento médio de ambas as strings.
+// function comprimentoMedioPalavras(a,b){
+//     return (a.length + b.length) / 2;
+       
+// }
+
+// console.log(comprimentoMedioPalavras('Pêra', 'Maçã'));
+
+
+ 
