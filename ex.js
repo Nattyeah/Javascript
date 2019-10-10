badbdab6c920fd911b0500e4f19f04c74a7a76d7
Nattyeah <53342856+Nattyeah@users.noreply.github.com>
2019-10-09 22:26:21 -0300

    Add files via upload

--- ex.js
+++ ex.js
@@ -54,13 +54,39 @@
 // console.log(credencialEhValida('Nataly', '12345678'));
 
 //7. Escreva uma função que receba uma array como parâmetro e devolva outra, mas sem elementos repetidos.
-const arr = [45, 765, 23, 76, 43, 65, 2345, 677, 5, 34, 22, 546, 77, 4355, 5667, 45, 56, 7, 4, 755, 45];
-function numerosVarios(array){
-for(i = 0; i <= array.length; i++){
-    console.log('loop i', array[i])
-    for(j = 1; j <= array.length; j++){
-        console.log('loop j', array[j])
-    }
-        }
-}
-console.log(numerosVarios(arr));
+
+// function numerosVarios(array){
+//     const arr = [];
+//     for(i = 0; i < array.length; i++){
+//         for(j = 1; j <= array.length; j++){
+
+//             if(array.indexOf(array[i] != arr.indexOf[j])){
+
+//         arr.push(array[i])
+//             }
+//             return  
+//         }
+//         }
+// }
+// console.log(numerosVarios([45, 765, 23, 76, 43, 65, 2345, 677, 5, 34, 22, 546, 77, 4355, 5667, 45, 56, 7, 4, 755, 45]));
+
+// VALIDAÇÃO DE CARTÃO DE CRÉDITO (ALGORITMO DE LUHN)
+// Nesse exercício, você fará a validação de números de cartão de crédito utilizando o algoritmo de Luhn:
+// 1) Retire o último dígito do número. Ele é o verificador;
+// 2) Escreva os números na ordem inversa;
+// 3) Multiplique os dígitos das casas ímpares por 2 e subtraia 9 de todos os resultados maiores que 9;
+// 4) Some todos os números;
+// 5) O dígito verificador (aquele do passo 1) é o número que você precisa somar a todos os outros números somados pra obter um módulo 10.
+// 6) O cartão será válido se os números somados % 10 === 0.
+let cartao = "4532841148586221";
+let resultado = cartao.split('')
+let ultimoDigito = resultado.pop()
+    resultado.reverse()
+    resultado.forEach(function digitosImpares(digitos){
+let i = 0;
+        if(i % 2 === 1){
+    return resultado[i] *= 2;
+            }
+        console.log(resultado)
+    })
+    
